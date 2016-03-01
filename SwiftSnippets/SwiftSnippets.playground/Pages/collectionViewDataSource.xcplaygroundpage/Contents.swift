29f88f7b23ce4e2a66b3b8fff8abaf793de6ba27
jueyingxx <jueyingxx@163.com>
2016-03-01 15:13:17 +0800

    add collectionViewDataSource.Playground

---
+++ SwiftSnippets/SwiftSnippets.playground/Pages/collectionViewDataSource.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+// MARK: - UICollectionViewDataSource
+
+func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return <#code#>
+}
+
+func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return <#code#>
+}
+
+func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    return <#code#>
+}
