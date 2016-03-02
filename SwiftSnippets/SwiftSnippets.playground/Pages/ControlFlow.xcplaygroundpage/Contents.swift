b8b35a3e52eb34d55cf2477c302beebbda299540
leacode <lichunyu@vip.qq.com>
2016-03-02 09:09:02 +0800

    add control flow snippet

---
+++ SwiftSnippets/SwiftSnippets.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+
+
+//: forin
+
+for <#object#> in <#collection#> {
+    
+}
+
+//: forLessThan
+
+for <#variable#> in <#number1#>..<<#number2#> {
+    
+}
+
+//: switchLoop
+
+switch <#val#> {
+case <#1#>:
+    
+    break
+case <#2#>:
+    
+    break
+default:
+    break
+}
+
+//: guardelse
+
+guard <#condition#> else {
+    <#returnOrthrow#>
+}
+
+//: trycatch
+
+do {
+    try <#expression#>
+} catch {
+    
+}
+
+//: [Next](@next)
